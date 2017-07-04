41a7d18b372b6338f9e2b35f556d55568125b63d
Daniel Gao <ganieldao@gmail.com>
2017-07-04 13:10:01 +0800

    Add fast html parser

--- src/Leedr.js
+++ src/Leedr.js
@@ -8,6 +8,24 @@ import {
 import {Provider} from 'react-redux';
 
 export default class Leedr extends Component {
+  componentWillMount() {
+
+    var HTMLParser = require('fast-html-parser');
+ 
+    var root = HTMLParser.parse('<ul id="list"><li>Hello World</li></ul>');
+ 
+    console.log(root.firstChild.structure);
+
+    const testUrl = 'https://royalroadl.com/fiction/1439/forgotten-conqueror';
+    fetch(testUrl)
+      .then(function(data) {
+        // Here you get the data to modify as you please
+      })
+      .catch(function(error) {
+        // If there is any error you will catch them here
+      }); 
+  }
+
   render() {
     return (
         <View>
