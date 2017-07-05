01f243a2afef18ff6bd2cb9985fe33c038251121
Daniel Gao <ganieldao@gmail.com>
2017-07-05 20:47:46 +0800

    Fetch all chapter links working

--- src/Leedr.js
+++ src/Leedr.js
@@ -11,21 +11,38 @@ const HTMLParser = require('fast-html-parser');
 
 export default class Leedr extends Component {
   componentWillMount() {
-
-    this.fetchChapters();
+    this.fetchHtmlSource('asdf')
+      .then((htmlString) => this.fetchChapters(htmlString))
+      .catch((error) => console.log(error));
   }
 
-  async fetchChapters() {
+  async fetchHtmlSource(url) {
     const testUrl = 'https://royalroadl.com/fiction/1439/forgotten-conqueror';
     console.log("time to fetch");
+    let body;
     try {
       const response = await fetch(testUrl);
-      var theDoc = HTMLParser.parse(response["_bodyInit"]);
-      //This returns the first chapter url
-      console.log(theDoc.querySelector('#chapters').querySelectorAll('a')[0].attributes["href"]);
+      body = response["_bodyInit"];
+      //console.log(body);
     } catch (err) {
       console.log('fetch failed', err);
-    }
+    } 
+    return body;
+  }
+
+  fetchChapters(htmlString) {
+    console.log("getting chapters");
+    var theDoc = HTMLParser.parse(htmlString);
+
+    //RRL has a table with the id 'chapters'. We are looking for the chapter links which are in the 'tbody'
+    //Each row of the 'tbody' is represented by 'tr', and the first 'a' contains the chapter link
+    var rows = theDoc.querySelector('#chapters').querySelector('tbody').querySelectorAll('tr')
+    var links = rows.map(function(row) {
+      return 'https://royalroadl.com' + row.querySelector('a').attributes["href"];
+    })
+
+    console.log(links);
+    return links
   }
 
   render() {
