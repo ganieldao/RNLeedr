e18546e1c8990cd060f8de7fd25e253a73a2cec5
Daniel Gao <ganieldao@gmail.com>
2017-07-18 22:31:28 +0800

    Forget BT for now

--- src/sources/common.js
+++ src/sources/common.js
@@ -1,11 +1,10 @@
-const HTMLParser = require('fast-html-parser');
-
 export async function fetchHtmlSource(url) {
   console.log("time to fetch", url);
   let body;
   try {
     const response = await fetch(url);
     body = response['_bodyInit'];
+    console.log('fetch success');
   } catch (err) {
     console.log('fetch failed', err);
   } 
