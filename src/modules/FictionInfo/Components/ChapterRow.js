522de20240713477ac02e5a552d2f8554ec20f1c
Daniel Gao <ganieldao@gmail.com>
2017-07-19 22:20:25 +0800

    Add temporary reading icon

--- src/modules/FictionInfo/Components/ChapterRow.js
+++ src/modules/FictionInfo/Components/ChapterRow.js
@@ -11,8 +11,6 @@ import {
   TouchableHighlight
 } from 'react-native';
 
-const icon = require('../../../res/one.png')
-
 const ChapterRow = ({onPressChapter, item, current}) => {
   let color = 'blue';
   if(item.content === '') {
@@ -20,13 +18,15 @@ const ChapterRow = ({onPressChapter, item, current}) => {
   }
 
   if(current) {
-    color = 'green';
+    icon = require('../../../res/in_progress.png');
+  } else {
+    icon = require('../../../res/one.png')
   }
 
   return (<TouchableHighlight underlayColor='gray' onPress={() => onPressChapter()}>
     <View style={{flex:1, marginTop:'1%', marginBottom:'1%', flexDirection:'row'}}>
       <View style={{flex: 0.1, justifyContent:'center', alignItems:'center'}}>
-        {(item.status === 'unread') && <Image source={icon}/>}
+        {(item.status === 'read' && !current) || <Image style={{flex:1, width:'80%', resizeMode:'contain'}} source={icon}/>}
       </View>
       <View style={{flex: 0.9, flexDirection:'column'}}>
         <Text style={{color:color, fontWeight:'bold'}}>{item.title}</Text>
